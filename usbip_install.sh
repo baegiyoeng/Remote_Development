ae002cbf96ad5178f60b6f7686b02e79fb55a08f
배기영 (GY Bae) <gy.bae@telechips.com>
2025-04-28 15:50:47 +0900

    [bug] Resolve error due to missing usb.ids file
     Fix missing usb.ids issue by adding manual installation
     procedure after usbutils package installation
    
    ITS: TMBSPG-1831

--- usbip_install.sh
+++ usbip_install.sh
@@ -5,4 +5,6 @@ pip install pyserial
 apt install linux-tools-$(uname -r)
 apt install linux-modules-extra-$(uname -r)
 apt install usbutils
+mkdir -p /usr/share/hwdata
+wget -O /usr/share/hwdata/usb.ids https://raw.githubusercontent.com/usbutils/usbutils/master/data/usb.ids
 apt install minicom
